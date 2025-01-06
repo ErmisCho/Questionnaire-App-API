bde1d9fa246c028f78ae0600dade88260f78590b
Ermis <ermis.chorinopoulos@gmail.com>
2025-01-06 08:48:23 +0100

    feat: added basic views for surveys and iterations

--- recruit/questionnaire/views.py
+++ recruit/questionnaire/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+from rest_framework import viewsets
+from .models import Survey, Iteration, Question, AnswerOption, Answer
+from .serializers import SurveySerializer, IterationSerializer, QuestionSerializer, AnswerOptionSerializer, AnswerSerializer
+
+
+class SurveyViewSet(viewsets.ModelViewSet):
+    # Manages surveys (CRUD operations).
+    queryset = Survey.objects.all()
+    serializer_class = SurveySerializer
+
+
+class IterationViewSet(viewsets.ModelViewSet):
+    # Manages iterations (CRUD operations).
+    queryset = Iteration.objects.all()
+    serializer_class = IterationSerializer
