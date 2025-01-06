60f5692825c5e3bb4539e16aa463e58fdeda97dd
Ermis <ermis.chorinopoulos@gmail.com>
2025-01-06 08:49:38 +0100

    feat: added url endpoints for surveys and iterations

--- recruit/recruit/urls.py
+++ recruit/recruit/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+from questionnaire.views import IterationViewSet, SurveyViewSet
+
+router = DefaultRouter()
+router.register(r'surveys', SurveyViewSet)
+router.register(r'iterations', IterationViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include(router.urls))
 ]
