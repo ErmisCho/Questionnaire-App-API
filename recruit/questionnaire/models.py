df6d2cad612f50bc54d6166cadb9593379fa4a92
Ermis <ermis.chorinopoulos@gmail.com>
2025-01-06 11:29:23 +0100

    doc: added docstrings for easier future understanding

--- recruit/questionnaire/models.py
+++ recruit/questionnaire/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class Survey(models.Model):
+    """Represents a survey containing multiple questions."""
     name = models.CharField(max_length=100)
     key = models.UUIDField(primary_key=True)  # for easier test files....
 
 
 class Question(models.Model):
+    """Represents a question belonging to a survey."""
     name = models.CharField(max_length=100, default="Question 1")
     text = models.TextField()
     survey = models.ForeignKey(
@@ -15,6 +17,7 @@ class Question(models.Model):
 
 
 class AnswerOption(models.Model):
+    """Represents an answer option for a specific question."""
     name = models.CharField(max_length=100, default="Option 1")
     key = models.UUIDField(primary_key=True)  # for easier test files....
     question = models.ForeignKey(
@@ -23,7 +26,9 @@ class AnswerOption(models.Model):
 
 
 class Iteration(models.Model):
-    # user as string is for simplification, since no user management system is implemented.
+    """Represents an iteration (attempt) of answering a survey."""
+
+    # Note: Keeping user as string is for simplification, since no user management system is implemented.
     user = models.CharField(max_length=100)
     key = models.UUIDField(primary_key=True)  # for easier test files....
     survey = models.ForeignKey(
@@ -32,6 +37,7 @@ class Iteration(models.Model):
 
 
 class Answer(models.Model):
+    """Represents an answer given in an iteration."""
     answer_option = models.ForeignKey(AnswerOption, on_delete=models.CASCADE)
     additional_information = models.TextField(null=True)
     iteration = models.ForeignKey(
